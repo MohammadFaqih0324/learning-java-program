6fce07a842f6aa48bdfb5809c3c7a63190e8a790
Mohamad Faqih <mohfaqihrahakbauw@gmail.com>
2024-05-15 23:23:12 +0700

    pengenalan print, println, printf dan alur eksekusi

---
+++ java-06-pengenalan-print-dan-alur-eksekusi/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // println, print, printf
+        System.out.println("Hello teman-teman");
+        System.out.print("baris ke-2 : Hello juga bro!!! ");
+
+        System.out.print("baris ke-3 : btw besok lu wfo kaga mas? \n");
+        System.out.print("baris ke-4 : wfo nih cuy, lu kaga wfo?");
+
+        System.out.println("baris ke-5 : wfo, ini masih dijalan bro");
+        System.out.printf("baris ke-6 : lah buset lama bener, udh jam %d lewat cuk", 9);
+
+        System.out.println("baris ke-7 : gua kejebak macet nih di cawang");
+        System.out.println("baris ke-8 : oh gitu iya-iya");
+        System.out.printf("baris ke-9 : paling gua sampe kantor jam %d haha", 11);
+
+    }
+}

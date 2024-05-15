b7af90d377bb73daa6e545e99abed748260af4ac
Mohamad Faqih <mohfaqihrahakbauw@gmail.com>
2024-05-16 01:20:51 +0700

    memahami string [] args

---
+++ java-07-memahami-string-args/src/com/learing/Main.java
@@ -0,0 +1,10 @@
+package com.learing;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hallo " + args[0] + "Ganteng");
+        System.out.println("Hallo " + args[1] + "Cantik");
+
+    }
+}

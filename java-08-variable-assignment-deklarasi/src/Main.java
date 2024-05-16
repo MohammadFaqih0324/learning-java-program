ed2097b98cc0674501a15a84d8b8135494fdc21b
Mohamad Faqih <mohfaqihrahakbauw@gmail.com>
2024-05-16 16:50:23 +0700

    memahami variable assignment dan deklarasi

---
+++ java-08-variable-assignment-deklarasi/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // belajar variable
+        int a = 5; // assigment
+        System.out.println("Nilai a = " + a);
+
+        a = 10;
+        System.out.println("Nilai a baru = " + a);
+
+        // membuat deklarasi
+        int b;
+
+        b = 15;
+
+        System.out.println("Nilai b = " + b);
+
+    }
+}

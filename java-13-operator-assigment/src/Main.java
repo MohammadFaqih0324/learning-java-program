c4a6b6905880613c06b3f90c2735032f3972e6df
Mohamad Faqih <mohfaqihrahakbauw@gmail.com>
2024-05-18 10:59:12 +0700

    operator assigmnet

---
+++ java-13-operator-assigment/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // operator assigment
+
+        // assigment operator penjumlahan
+        int a = 1;
+        a += 10; // a = a + 10;
+        System.out.println("nilai a = " + a);
+
+        // assigment operator pengurangan
+        int b = 100;
+        b -= 85;
+        System.out.println("nilai b = " + b);
+
+        // assigment operator perkalian
+        int c = 60;
+        c *= 100;
+        System.out.println("nilai c = " + c);
+
+        // assigment operator pembagian
+        int d = 100;
+        d /= 20;
+        System.out.println("nilai d = " + d);
+
+        // assigment operator modulus
+        int e = 22;
+        e %= 7;
+        System.out.println("nilai e = " + e);
+
+    }
+}

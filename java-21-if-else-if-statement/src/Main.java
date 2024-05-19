f683280c77bdb850962c64c1ac63eb7afd1b51c0
Mohamad Faqih <mohfaqihrahakbauw@gmail.com>
2024-05-19 14:03:44 +0700

    if else if statement

---
+++ java-21-if-else-if-statement/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // if else if statement
+        int a = 5;
+
+        System.out.println("ini adalah awal program");
+
+        if (a == 7){
+            System.out.println("ini adalah aksi 1");
+        } else if (a == 5){
+            System.out.println("ini adalah aksi 2");
+        } else {
+            System.out.println("ini adalah aksi deafault");
+        }
+        // akhir if else if statement
+        System.out.println("ini adalah akhir program");
+    }
+}

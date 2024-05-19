88075d9746ea750db46c9384d3d4327a56b8d37f
Mohamad Faqih <mohfaqihrahakbauw@gmail.com>
2024-05-19 13:55:16 +0700

    if else statement

---
+++ java-20-if-else-statement/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static  void  main(String[] args){
+
+        int a = 9;
+
+        System.out.println("nilai = " + a);
+
+        // kondisinya / statement
+        // nilai ekspresinya itu boolean
+        if (a == 10) { // ekpresi
+            System.out.println("ini adalah jalur true"); // aksi
+        } else {
+            System.out.println("ini adalah jalur false");
+        }
+        System.out.println("selesai");
+    }
+}
